6f676f42f7f73fdc3e27064d9045c7e173725d15
asulon <asulon@student.42.fr>
2026-04-02 17:14:53 +0200

    add new ex0 with new subject

---
+++ ex1/ft_raise_exception.py
@@ -0,0 +1,44 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  ft_raise_exception.py                             :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: asulon <asulon@student.42.fr>             +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/04/02 16:06:57 by asulon          #+#    #+#               #
+#  Updated: 2026/04/02 17:12:36 by asulon          ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+def input_temperature(temp_str: str) -> int:
+    try:
+        temperature = int(temp_str)
+    except ValueError as error:
+        raise ValueError(f"{error}\n")
+    if temperature > 40:
+        raise ValueError(
+            f"{temp_str}°C is too hot for plants (max 40°C)\n")
+    if temperature < 0:
+        raise ValueError(
+            f"{temp_str}°C is too cold for plants (min 0°C)\n")
+    return temperature
+
+
+def test_temperature() -> None:
+    print("=== Garden Temperature Checker ===")
+    inputs = ["25", "abc", "100", "-50"]
+
+    for temp_str in inputs:
+        print(f"Input data is '{temp_str}'")
+        try:
+            temperature = input_temperature(temp_str)
+            temperature = int(temperature)
+            print(f"Temperature is now {temperature}°C\n")
+        except ValueError as error:
+            print(f"Caught input_temperature error: {error}")
+
+    print("All tests completed - program didn't crash!")
+
+
+if __name__ == "__main__":
+    test_temperature()
