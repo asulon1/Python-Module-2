721be9058282b9cfb2e9ceead2475d87ca2ad506
asulon <asulon@student.42.fr>
2026-04-02 16:09:39 +0200

    ex0 remove unused code

--- ex0/ft_first_exception.py
+++ ex0/ft_first_exception.py
@@ -6,28 +6,21 @@
 #  By: asulon <asulon@student.42.fr>             +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/02/19 17:51:59 by asulon          #+#    #+#               #
-#  Updated: 2026/04/02 16:05:13 by asulon          ###   ########.fr        #
+#  Updated: 2026/04/02 16:09:12 by asulon          ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
 def input_temperature(temp_str: str) -> int:
     try:
         temperature = int(temp_str)
-    except ValueError:
-        raise ValueError(
-            f"Caught input_temperature error: '{temp_str}' is not a valid number\n")
-    if temperature > 40:
-        raise ValueError(
-            f"Caught input_temperature error: {temp_str}°C is too hot for plants (max 40°C)\n")
-    if temperature < 0:
-        raise ValueError(
-            f"Caught input_temperature error: {temp_str}°C is too cold for plants (min 0°C)\n")
+    except ValueError as error:
+        raise ValueError(error)
     return temperature
 
 
 def test_temperature() -> None:
     print("=== Garden Temperature Checker ===")
-    inputs = ["25", "abc", "100", "-50"]
+    inputs = ["25", "abc"]
 
     for temp_str in inputs:
         print(f"Input data is '{temp_str}'")
@@ -38,7 +31,7 @@ def test_temperature() -> None:
         except ValueError as error:
             print(f"{error}")
 
-    print("All tests completed - program didn't crash!")
+    print("\nAll tests completed - program didn't crash!")
 
 
 if __name__ == "__main__":
