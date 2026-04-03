260fec2c224e74f44bbcb644415624f1f677bfe2
asulon <asulon@student.42.fr>
2026-04-03 16:46:32 +0200

    end ex4 with new subject

--- ex4/ft_finally_block.py
+++ ex4/ft_finally_block.py
@@ -6,40 +6,55 @@
 #  By: asulon <asulon@student.42.fr>             +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/02/23 19:48:12 by asulon          #+#    #+#               #
-#  Updated: 2026/04/02 18:27:29 by asulon          ###   ########.fr        #
+#  Updated: 2026/04/03 16:45:53 by asulon          ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
 class GardenError(Exception):
     def __init__(self, message: str = "A garden error occurred!") -> None:
-        super().__init__(message)
+        super().__init__("Caught " + message)
 
 
 class PlantError(GardenError):
-    def __init__(self, name: str = "Unknown", message: str = "Plant is wilting!") -> None:
-        super().__init__(name + ' ' + message)
+    def __init__(self, message: str = "Plant is wilting!") -> None:
+        super().__init__(f"PlantError: {message}")
 
 
 def water_plant(plant_name: str) -> None:
     try:
-        if not plant_name.capitalize():
-            raise ValueError(f"Cannot water {plant} - invalid plant!")
-        print(f"Watering {plant}: [OK]")
+        if plant_name[0].isupper():
+            print(f"Watering {plant_name}: [OK]")
+        else:
+            raise PlantError(f"Invalid plant name to water: '{plant_name}'")
     except ValueError as error:
-        print(f"Error: {error}")
-    finally:
-        print("Closing watering system (cleanup)")
-    print("Watering completed successfully!\n")
+        raise PlantError(f"{error}")
 
 
 def test_watering_system():
+    valid_list = ["Tomato", "Lettuce", "Carrots"]
+    invalid_list = ["Tomato", "lettuce"]
+    print("=== Garden Watering System ===\n")
     print("Testing valid plants...")
     print("Opening watering system")
-    print("")
-    print("=== Garden Watering System ===\n")
+    try:
+        for plant in valid_list:
+            water_plant(plant)
+    except ValueError as error:
+        print(f"Error: {error}")
+    finally:
+        print("Closing watering system\n")
 
-    water_plant()
-    print("All custom error types work correctly!")
+    print("Testing invalid plants...")
+    print("Opening watering system")
+    try:
+        for plant in invalid_list:
+            water_plant(plant)
+    except PlantError as error:
+        print(error)
+        print(".. ending tests and returning to main")
+    finally:
+        print("Closing watering system\n")
+    print("Cleanup always happens, even with errors!")
 
 
 if __name__ == "__main__":
