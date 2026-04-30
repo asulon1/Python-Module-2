6db77aa27df81e679fd0c7ef450090a6c5987fc9
asulon <asulon@student.42.fr>
2026-04-30 16:03:09 +0200

    ex0 new subject changes

--- ex0/ft_first_exception.py
+++ ex0/ft_first_exception.py
@@ -6,27 +6,22 @@
 #  By: asulon <asulon@student.42.fr>             +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/02/19 17:51:59 by asulon          #+#    #+#               #
-#  Updated: 2026/04/02 17:12:45 by asulon          ###   ########.fr        #
+#  Updated: 2026/04/30 16:01:49 by asulon          ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
 def input_temperature(temp_str: str) -> int:
-    try:
-        temperature = int(temp_str)
-    except ValueError as error:
-        raise ValueError(f"{error}\n")
-    return temperature
+    return int(temp_str)
 
 
 def test_temperature() -> None:
-    print("=== Garden Temperature Checker ===")
+    print("=== Garden Temperature ===")
     inputs = ["25", "abc"]
 
     for temp_str in inputs:
         print(f"Input data is '{temp_str}'")
         try:
             temperature = input_temperature(temp_str)
-            temperature = int(temperature)
             print(f"Temperature is now {temperature}°C\n")
         except ValueError as error:
             print(f"Caught input_temperature error: {error}")
